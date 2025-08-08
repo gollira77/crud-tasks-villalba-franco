7338f7454ff90832f39ecaf0184b84928e958e03
Franco <franquivillalba13@gmail.com>
2025-08-08 15:44:47 -0300

    creación de los modelos Task y User que se conectan a la base de datos

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import sequelize from './src/config/database.js';
+import User from './src/models/user.model.js';
+import Task from './src/models/task.model.js';
 
 dotenv.config();
 
@@ -10,12 +12,15 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 (async () => {
-    try{
-        await sequelize.authenticate();
-        console.log("Conexión exictosa con la DB")
-    }catch(error) {
-        console.error("No hubo conexión con la BD por:",error)
-    }
+  try {
+    await sequelize.authenticate();
+    console.log("Conexión exictosa con la DB");
+    await sequelize.sync({ alter: true }); 
+    console.log("Tablas sincronizadas correctamente.");
+    
+  } catch (error) {
+    console.error("No hubo conexión con la BD por:", error);
+  }
 })();
 
 app.listen(PORT, () => {
