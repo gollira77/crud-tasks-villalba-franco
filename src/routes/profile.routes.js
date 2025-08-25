62002d01f3b1f57b74b08590e233dc5a8f693731
Franco <franquivillalba13@gmail.com>
2025-08-25 06:52:42 -0300

    CRUD completo con validaciones y eliminación en cascada con Profile

--- src/routes/profile.routes.js
+++ src/routes/profile.routes.js
@@ -1,9 +1,45 @@
 import { Router } from "express";
-import { createProfile, getAllProfiles} from "../controllers/profile.controllers.js";
+import { body, param } from "express-validator";
+import {createProfile, getAllProfiles, getProfileById, updateProfile, deleteProfile} from "../controllers/profile.controllers.js";
+import { validateFields } from "../middlewares/validation.middleware.js";
 
 const router = Router();
 
-router.post("/", createProfile);
-router.get("/", getAllProfiles); 
+router.post(
+  "/",
+  [
+    body("userId").isInt().withMessage("El userId debe ser un número entero"),
+    body("bio").optional().isLength({ max: 255 }),
+    body("avatar").optional().isLength({ max: 255 }),
+    validateFields,
+  ],
+  createProfile
+);
+
+router.get("/", getAllProfiles);
+
+router.get(
+  "/:id",
+  [param("id").isInt().withMessage("El ID debe ser un número entero"), validateFields],
+  getProfileById
+);
+
+router.put(
+  "/:id",
+  [
+    param("id").isInt().withMessage("El ID debe ser un número entero"),
+    body("userId").optional().isInt(),
+    body("bio").optional().isLength({ max: 255 }),
+    body("avatar").optional().isLength({ max: 255 }),
+    validateFields,
+  ],
+  updateProfile
+);
+
+router.delete(
+  "/:id",
+  [param("id").isInt().withMessage("El ID debe ser un número entero"), validateFields],
+  deleteProfile
+);
 
 export default router;
